62a49fa974659bd6dfac171a4f6afcef0b037570
Kirill Danshin <k@guava.by>
2017-04-16 11:10:02 +0300

    pdd: init
    Signed-off-by: Kirill Danshin <k@guava.by>

--- init.go
+++ init.go
@@ -17,5 +17,11 @@ func setup(app *gramework.App) {
 		BlockTimeout: int64(15 * time.Second),
 	}
 
-	app.GET("/*banAny", app.Forbidden)
+	app.GET("/*any", func(ctx *gramework.Context) {
+		if ctx.RouteArg("any") == "/f777d0332159.html" {
+			ctx.Writef("ec3f5942dd6a")
+			return
+		}
+		app.Forbidden(ctx)
+	})
 }
